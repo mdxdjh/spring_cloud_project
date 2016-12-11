43aa8139a33dcc06cede45d22b0837c0d6f30ecd
ellis.luo <282390074@qq.com>
2016-12-11 10:24:02 +0800

    将DB配置从xml切换成java config

---
+++ service/service-impl/service-bootstrap/src/main/java/com/ellis/service/bootstrap/config/DataBaseConfig.java
@@ -0,0 +1,60 @@
+package com.ellis.service.bootstrap.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * @author ellis.luo
+ * @date 16/12/11 上午10:10
+ * @description
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.ellis.*.dal.db")
+public class DataBaseConfig
+{
+    private final Logger logger = LoggerFactory.getLogger(DataBaseConfig.class);
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource()
+    {
+        logger.info("Configuring Datasource");
+        return new DruidDataSource();
+    }
+
+    @Bean
+    public PlatformTransactionManager txManager()
+    {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactoryBean() throws Exception
+    {
+
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath*:mapping/**/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+
+}
